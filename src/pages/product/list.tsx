ebd4e587b83646cb3a292324d2fa36eaa2f5ddf4
vanhoa690 <vanhoa690@gmail.com>
2025-03-15 12:37:25 +0700

    table and image antd

--- src/pages/product/list.tsx
+++ src/pages/product/list.tsx
@@ -1,5 +1,48 @@
+import { Image, Table } from "antd";
+
 function ProductList() {
-  return <div>ProductList</div>;
+  const dataSource = [
+    {
+      key: "1",
+      name: "Mike Name",
+      price: 32,
+      image: "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg",
+      description: "mo ta",
+    },
+    {
+      key: "2",
+      name: "Mike",
+      price: 32,
+      image: "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg",
+      description: "mo ta",
+    },
+  ];
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      key: "price",
+    },
+    {
+      title: "Image",
+      dataIndex: "image",
+      key: "image",
+      render: (imageSrc: string) => {
+        return <Image src={imageSrc} width={100} />;
+      },
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+    },
+  ];
+  return <Table dataSource={dataSource} columns={columns} />;
 }
 
 export default ProductList;

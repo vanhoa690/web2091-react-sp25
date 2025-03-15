6da5a25c6154a387b220e1390c532778045ebeb1
vanhoa690 <vanhoa690@gmail.com>
2025-03-15 07:57:39 +0700

    table antd

--- src/pages/product/list.tsx
+++ src/pages/product/list.tsx
@@ -1,7 +1,50 @@
+import { Image, Table } from "antd";
+
 // function component
 function ProductList() {
-  // JSX
-  return <div>ProductList</div>;
+  // call API ve
+  const products = [
+    {
+      key: "1",
+      name: "San pham A",
+      price: 32,
+      image: "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg",
+      desc: "mo ta",
+    },
+    {
+      key: "2",
+      name: "San pham A",
+      price: 32,
+      image: "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg",
+      desc: "mo ta",
+    },
+  ];
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      key: "price",
+    },
+    {
+      title: "Image",
+      dataIndex: "image",
+      key: "image",
+      render: (image: string) => {
+        return <Image src={image} width={100} />;
+      },
+    },
+    {
+      title: "Description",
+      dataIndex: "desc",
+      key: "desc",
+    },
+  ];
+  return <Table dataSource={products} columns={columns} />;
 }
 
 export default ProductList;

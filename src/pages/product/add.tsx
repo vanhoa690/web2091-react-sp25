b2c632e89cb6b4e107ed0384c2b16349838a989f
vanhoa690 <vanhoa690@gmail.com>
2025-03-18 12:59:19 +0700

    add product

--- src/pages/product/add.tsx
+++ src/pages/product/add.tsx
@@ -1,17 +1,29 @@
 import { useMutation } from "@tanstack/react-query";
-import { Button, Form, Input, InputNumber } from "antd";
+import { Button, Form, Input, InputNumber, message } from "antd";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+type ProductForm = {
+  name: string;
+  price: number;
+};
 
 function ProductAdd() {
-  const addProduct = async (data: any) => {
+  const nav = useNavigate();
+  const addProduct = async (data: ProductForm) => {
     await axios.post("http://localhost:3000/products", data);
   };
 
   const { mutate } = useMutation({
     mutationFn: addProduct,
+    onSuccess: () => {
+      message.success("them thanh cong");
+      nav("/product/list");
+    },
+    onError: () => {},
   });
 
-  function onFinish(values: any) {
+  function onFinish(values: ProductForm) {
     mutate(values);
   }
   return (

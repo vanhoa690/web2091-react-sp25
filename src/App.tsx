8634017521455b4c4dec15d022e05da2b61c19b7
vanhoa690 <vanhoa690@gmail.com>
2025-03-18 12:28:36 +0700

    Form Antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { useRoutes } from "react-router";
-import "./App.css";
 import ProductList from "./pages/product/list";
 import ProductAdd from "./pages/product/add";
 import ProductEdit from "./pages/product/edit";
+import { Layout, Menu } from "antd";
+import "antd/dist/reset.css"; // Ant Design v5 reset styles
+import { Link } from "react-router-dom";
+
+const { Header } = Layout;
 
 function App() {
   const element = useRoutes([
@@ -19,7 +23,34 @@ function App() {
       element: <ProductEdit />,
     },
   ]);
-  return <div>{element}</div>;
+  return (
+    <div>
+      <Header style={{ display: "flex", alignItems: "center" }}>
+        <div style={{ color: "white", fontSize: "20px", fontWeight: "bold" }}>
+          <Link to="/">React Antd</Link>
+        </div>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["1"]}
+          style={{ marginLeft: "auto" }}
+        >
+          <Menu.Item key="1">
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/product/list">Products</Link>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Link to="/product/add">Add Product</Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <div className="container" style={{ maxWidth: 800, margin: "auto" }}>
+        {element}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/product/add.tsx
+++ src/pages/product/add.tsx
@@ -1,5 +1,30 @@
+import { Button, Form, Input, InputNumber } from "antd";
+
 function ProductAdd() {
-  return <div>ProductAdd</div>;
+  function onFinish(values: any) {
+    console.log(values);
+  }
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 30,
+        marginTop: 30,
+      }}
+    >
+      ProductAdd
+      <Form onFinish={onFinish}>
+        <Form.Item label="Name" name="name" rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item label="Price" name="price" rules={[{ required: true }]}>
+          <InputNumber />
+        </Form.Item>
+        <Button htmlType="submit">Submit</Button>
+      </Form>
+    </div>
+  );
 }
 
 export default ProductAdd;

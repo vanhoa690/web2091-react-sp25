3419ae238f752f64300bd4c9224297fdc162f5cd
vanhoa690 <vanhoa690@gmail.com>
2025-03-18 10:20:48 +0700

    add product useMutation

--- src/pages/product/add.tsx
+++ src/pages/product/add.tsx
@@ -1,8 +1,22 @@
-import { Button, Form, Input, InputNumber } from "antd";
+import { useMutation } from "@tanstack/react-query";
+import { Button, Form, Input, InputNumber, message } from "antd";
+import axios from "axios";
 
 function ProductAdd() {
+  const addProduct = async (data: any) => {
+    await axios.post("http://localhost:3000/products", data);
+  };
+
+  const { mutate } = useMutation({
+    mutationFn: addProduct,
+    onSuccess: () => {
+      message.success("Them san pham thanh cong");
+    },
+  });
+
   function onFinish(values: any) {
     console.log(values);
+    mutate(values);
   }
   return (
     <div
@@ -15,10 +29,14 @@ function ProductAdd() {
     >
       ProductAdd
       <Form onFinish={onFinish}>
-        <Form.Item label="Product Name" name="name">
+        <Form.Item label="Name" name="name" rules={[{ required: true }]}>
           <Input />
         </Form.Item>
-        <Form.Item label="Product Price" name="price">
+        <Form.Item
+          label="Price"
+          name="price"
+          rules={[{ required: true }, { type: "number", min: 0 }]}
+        >
           <InputNumber />
         </Form.Item>
         <Button htmlType="submit">Submit</Button>

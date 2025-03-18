1b5f89a68ecdecd95dc10b7c0bd3058ac3560adc
vanhoa690 <vanhoa690@gmail.com>
2025-03-18 09:47:12 +0700

    use Form Antd

--- src/pages/product/add.tsx
+++ src/pages/product/add.tsx
@@ -1,7 +1,30 @@
-// function component
+import { Button, Form, Input, InputNumber } from "antd";
+
 function ProductAdd() {
-  // JSX
-  return <div>ProductAdd</div>;
+  function onFinish(values: any) {
+    console.log(values);
+  }
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 30,
+        marginTop: 30,
+      }}
+    >
+      ProductAdd
+      <Form onFinish={onFinish}>
+        <Form.Item label="Product Name" name="name">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Product Price" name="price">
+          <InputNumber />
+        </Form.Item>
+        <Button htmlType="submit">Submit</Button>
+      </Form>
+    </div>
+  );
 }
 
 export default ProductAdd;

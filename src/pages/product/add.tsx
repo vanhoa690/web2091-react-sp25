ec53a196104196378336a985f231c3df26a37c82
vanhoa690 <vanhoa690@gmail.com>
2025-03-18 12:55:55 +0700

    mutate addProduct

--- src/pages/product/add.tsx
+++ src/pages/product/add.tsx
@@ -1,8 +1,18 @@
+import { useMutation } from "@tanstack/react-query";
 import { Button, Form, Input, InputNumber } from "antd";
+import axios from "axios";
 
 function ProductAdd() {
+  const addProduct = async (data: any) => {
+    await axios.post("http://localhost:3000/products", data);
+  };
+
+  const { mutate } = useMutation({
+    mutationFn: addProduct,
+  });
+
   function onFinish(values: any) {
-    console.log(values);
+    mutate(values);
   }
   return (
     <div
@@ -18,7 +28,11 @@ function ProductAdd() {
         <Form.Item label="Name" name="name" rules={[{ required: true }]}>
           <Input />
         </Form.Item>
-        <Form.Item label="Price" name="price" rules={[{ required: true }]}>
+        <Form.Item
+          label="Price"
+          name="price"
+          rules={[{ required: true }, { type: "number", min: 0 }]}
+        >
           <InputNumber />
         </Form.Item>
         <Button htmlType="submit">Submit</Button>

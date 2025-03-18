171119211514815f13febe90c68957f54f83f923
vanhoa690 <vanhoa690@gmail.com>
2025-03-18 08:17:46 +0700

    antd form

--- src/pages/product/add.tsx
+++ src/pages/product/add.tsx
@@ -1,5 +1,32 @@
+import { Button, Form, Input } from "antd";
+
 function ProductAdd() {
-  return <div>ProductAdd</div>;
+  function onFinish(values: any) {
+    console.log(values);
+  }
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 30,
+        marginTop: 30,
+      }}
+    >
+      ProductAdd
+      <Form onFinish={onFinish}>
+        <Form.Item label="Name" name="name">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Price" name="price">
+          <Input />
+        </Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default ProductAdd;

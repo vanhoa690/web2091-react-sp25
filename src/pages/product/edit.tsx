f0398d0801239810b08db8629a4808b47ba3f216
vanhoa690 <vanhoa690@gmail.com>
2025-03-20 12:42:51 +0700

    get product detail theo id

--- src/pages/product/edit.tsx
+++ src/pages/product/edit.tsx
@@ -1,7 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
 import { Button, Form, Input, InputNumber } from "antd";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 function ProductEdit() {
   // lay data theo id ve : useQuery + useParams lay id va data
+  const { id } = useParams();
+  const getProductDetail = async () => {
+    if (!id) return;
+    const { data } = await axios.get(`http://localhost:3000/products/${id}`);
+    return data;
+  };
+  const { data } = useQuery({
+    queryKey: ["product"],
+    queryFn: getProductDetail,
+  });
+  console.log(data);
+
   // data vao form : useForm trong Form cua Antd
   // onFinish = useMutation
   return (

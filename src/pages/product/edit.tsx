a1694fb05afac87b8b60e18ef1a99fb33762627b
vanhoa690 <vanhoa690@gmail.com>
2025-03-20 12:53:01 +0700

    data vao form

--- src/pages/product/edit.tsx
+++ src/pages/product/edit.tsx
@@ -1,28 +1,35 @@
 import { useQuery } from "@tanstack/react-query";
 import { Button, Form, Input, InputNumber } from "antd";
 import axios from "axios";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function ProductEdit() {
   // lay data theo id ve : useQuery + useParams lay id va data
   const { id } = useParams();
+  const [form] = Form.useForm();
+
   const getProductDetail = async () => {
     if (!id) return;
     const { data } = await axios.get(`http://localhost:3000/products/${id}`);
     return data;
   };
-  const { data } = useQuery({
+  const { data: product } = useQuery({
     queryKey: ["product"],
     queryFn: getProductDetail,
   });
-  console.log(data);
 
   // data vao form : useForm trong Form cua Antd
+  useEffect(() => {
+    if (!product) return;
+    form.setFieldsValue(product);
+  }, [product]);
+
   // onFinish = useMutation
   return (
     <div>
       ProductEdit
-      <Form>
+      <Form form={form}>
         <Form.Item label="Name" name="name" rules={[{ required: true }]}>
           <Input />
         </Form.Item>

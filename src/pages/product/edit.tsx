525ef0dbe851122c72e0fc1f07d5e4102aa8d1a4
vanhoa690 <vanhoa690@gmail.com>
2025-03-20 12:16:02 +0700

    product Edit

--- src/pages/product/edit.tsx
+++ src/pages/product/edit.tsx
@@ -1,5 +1,24 @@
+import { Button, Form, Input, InputNumber } from "antd";
+
 function ProductEdit() {
-  return <div>ProductEdit</div>;
+  return (
+    <div>
+      ProductEdit
+      <Form>
+        <Form.Item label="Name" name="name" rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Price"
+          name="price"
+          rules={[{ required: true }, { type: "number", min: 0 }]}
+        >
+          <InputNumber />
+        </Form.Item>
+        <Button htmlType="submit">Submit</Button>
+      </Form>
+    </div>
+  );
 }
 
 export default ProductEdit;

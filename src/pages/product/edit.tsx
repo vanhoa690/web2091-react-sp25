def2c6fafe91fe5c53b137a83f67e55663aec0f6
vanhoa690 <vanhoa690@gmail.com>
2025-03-20 10:08:08 +0700

    do data vao form thong qua useForm

--- src/pages/product/edit.tsx
+++ src/pages/product/edit.tsx
@@ -1,28 +1,37 @@
 import { useQuery } from "@tanstack/react-query";
 import { Button, Form, Input, InputNumber } from "antd";
 import axios from "axios";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function ProductEdit() {
   // get data product theo id
   const { id } = useParams();
+  const [form] = Form.useForm();
+
   const getProductDetail = async () => {
     if (!id) return;
     const { data } = await axios.get(`http://localhost:3000/products/${id}`);
     return data;
   };
-  const { data } = useQuery({
+  const { data: product } = useQuery({
     queryKey: ["product"],
     queryFn: getProductDetail,
   });
-  console.log({ data });
+  console.log({ product });
+
+  useEffect(() => {
+    if (!product) return;
+    form.setFieldsValue(product);
+  }, [product]);
 
   // dien data vao form
+  // useForm trong Form Antd
   // onFinish
   return (
     <div>
       ProductEdit
-      <Form>
+      <Form form={form}>
         <Form.Item label="Name" name="name" rules={[{ required: true }]}>
           <Input />
         </Form.Item>

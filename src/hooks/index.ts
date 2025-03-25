a57a03409317d1cf4ddaa0c28de924d6de1cf6a3
vanhoa690 <vanhoa690@gmail.com>
2025-03-25 12:58:52 +0700

    hook useList

---
+++ src/hooks/index.ts
@@ -0,0 +1,11 @@
+// Hook
+
+import { useQuery } from "@tanstack/react-query";
+import { getList } from "../providers";
+
+export const useList = ({ resource = "products" }) => {
+  return useQuery({
+    queryKey: [resource],
+    queryFn: () => getList({ resource }),
+  });
+};

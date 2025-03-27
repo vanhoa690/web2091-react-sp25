70828a1850a678fcf85a3bb6d8e7346dc0fbf0a4
vanhoa690 <vanhoa690@gmail.com>
2025-03-27 10:10:20 +0700

    register

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,3 +1,4 @@
+import { Button, Form, Input } from "antd";
 import { useAuth } from "../../hooks";
 
 function Register() {
@@ -15,6 +16,20 @@ function Register() {
       }}
     >
       Register
+      <Form onFinish={onFinish}>
+        <Form.Item label="Email" name="email">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Password" name="password">
+          <Input.Password />
+        </Form.Item>
+        <Form.Item label="Confirm Password">
+          <Input.Password />
+        </Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }

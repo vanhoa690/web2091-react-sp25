adfe14d27f3074b1e97429fa48162d414c197f97
vanhoa690 <vanhoa690@gmail.com>
2025-03-29 07:25:40 +0700

    add user list

--- src/pages/user/list.tsx
+++ src/pages/user/list.tsx
@@ -1,60 +1,22 @@
-import { Button, Image, Popconfirm, Space, Table } from "antd";
-import { Link } from "react-router-dom";
-import { useDelete, useList } from "../../hooks";
+import { Table } from "antd";
+import { useList } from "../../hooks";
 
-function ProductList() {
+function UserList() {
   const { data, isLoading } = useList({
     resource: "users",
   });
-  const { mutate } = useDelete({ resource: "users" });
 
   const columns = [
     {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
+      title: "Id",
+      dataIndex: "id",
     },
     {
-      title: "Price",
-      dataIndex: "price",
-      key: "price",
-    },
-    {
-      title: "Image",
-      dataIndex: "image",
-      key: "image",
-      render: (imageSrc: string) => {
-        return <Image src={imageSrc} width={100} />;
-      },
-    },
-    {
-      title: "Description",
-      dataIndex: "desc",
-      key: "desc",
-    },
-    {
-      title: "Actions",
-      render: (product: any) => {
-        return (
-          <Space>
-            <Button type="primary">
-              <Link to={`/product/${product.id}/edit`}>Edit</Link>
-            </Button>
-            <Popconfirm
-              title="Delete the task"
-              description="Are you sure to delete this task?"
-              onConfirm={() => mutate(product.id)}
-              okText="Cos"
-              cancelText="ko"
-            >
-              <Button danger>Delete</Button>
-            </Popconfirm>
-          </Space>
-        );
-      },
+      title: "Email",
+      dataIndex: "email",
     },
   ];
   return <Table dataSource={data} columns={columns} loading={isLoading} />;
 }
 
-export default ProductList;
+export default UserList;

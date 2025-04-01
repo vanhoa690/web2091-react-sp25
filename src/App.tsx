ecb1031ff34424209cbb45453fd16c1a45ad62f2
vanhoa690 <vanhoa690@gmail.com>
2025-04-01 07:56:21 +0700

    layout admin

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,33 @@ import { Link } from "react-router-dom";
 import Register from "./pages/auth/register";
 import UserList from "./pages/user/list";
 import Login from "./pages/auth/login";
+import AdminLayout from "./layout/Admin";
 
 const { Header } = Layout;
 
 function App() {
   const element = useRoutes([
     {
-      path: "/register",
-      element: <Register />,
+      path: "/admin",
+      element: <AdminLayout />,
+      children: [
+        {
+          path: "products",
+          element: <ProductList />,
+        },
+        {
+          path: "product/add",
+          element: <ProductAdd />,
+        },
+        {
+          path: "product/:id/edit",
+          element: <ProductEdit />,
+        },
+        {
+          path: "users",
+          element: <UserList />,
+        },
+      ],
     },
     {
       path: "/login",
@@ -25,45 +44,34 @@ function App() {
       path: "/users",
       element: <UserList />,
     },
-    {
-      path: "/products",
-      element: <ProductList />,
-    },
-    {
-      path: "/product/add",
-      element: <ProductAdd />,
-    },
-    {
-      path: "/product/:id/edit",
-      element: <ProductEdit />,
-    },
   ]);
   return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <Header>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1">
-            <Link to="/">React Antd</Link>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Link to="/products">Products</Link>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Link to="/product/add">Add Product</Link>
-          </Menu.Item>
-          <Menu.Item key="4">
-            <Link to="/register">Register</Link>
-          </Menu.Item>
-          <Menu.Item key="5">
-            <Link to="/users">User List</Link>
-          </Menu.Item>
-          <Menu.Item key="6">
-            <Link to="/login">Login</Link>
-          </Menu.Item>
-        </Menu>
-      </Header>
-      <main>{element}</main>
-    </Layout>
+    <main>{element}</main>
+    // <Layout style={{ minHeight: "100vh" }}>
+    //   <Header>
+    //     <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
+    //       <Menu.Item key="1">
+    //         <Link to="/">React Antd</Link>
+    //       </Menu.Item>
+    //       <Menu.Item key="2">
+    //         <Link to="/products">Products</Link>
+    //       </Menu.Item>
+    //       <Menu.Item key="3">
+    //         <Link to="/product/add">Add Product</Link>
+    //       </Menu.Item>
+    //       <Menu.Item key="4">
+    //         <Link to="/register">Register</Link>
+    //       </Menu.Item>
+    //       <Menu.Item key="5">
+    //         <Link to="/users">User List</Link>
+    //       </Menu.Item>
+    //       <Menu.Item key="6">
+    //         <Link to="/login">Login</Link>
+    //       </Menu.Item>
+    //     </Menu>
+    //   </Header>
+
+    // </Layout>
   );
 }
 

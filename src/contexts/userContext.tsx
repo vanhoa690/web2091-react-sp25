4ed4066b2fcb0b18c0b9152baefef5fb8b18e159
vanhoa690 <vanhoa690@gmail.com>
2025-04-03 08:06:58 +0700

    userContext

--- src/contexts/userContext.tsx
+++ src/contexts/userContext.tsx
@@ -1,9 +1,32 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-export const UserContext = createContext(null);
+type User = {
+  id: number;
+  email: string;
+  role?: string;
+};
+
+type UserContextType = {
+  user: User | null;
+  login: (user: User) => void;
+  logout: () => void;
+};
+
+export const UserContext = createContext<UserContextType | null>(null);
 
 export const UserProvider = ({ children }: { children: any }) => {
-  const user = { name: "Hoadv", email: "hoadv@example.com" };
+  const [user, setUser] = useState<User | null>(null);
 
-  return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+  const login = (user: User) => {
+    setUser(user);
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+  return (
+    <UserContext.Provider value={{ user, login, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
 };

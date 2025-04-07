76cd7b2c0d2c0f14193addd18dce2c43a19367e6
vanhoa690 <vanhoa690@gmail.com>
2025-04-07 20:23:42 +0700

    add env

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/",
   headers: {
     Authorization: token && `Bearer ${token}`,
   },

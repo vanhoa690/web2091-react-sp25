3714cd1a1093934a81a868b6d4e98bfc122c6e1a
vanhoa690 <vanhoa690@gmail.com>
2025-04-11 15:49:51 +0700

    test query

--- src/pages/product/list.tsx
+++ src/pages/product/list.tsx
@@ -1,13 +1,64 @@
-import { Button, Image, Popconfirm, Space, Table } from "antd";
+import { Button, Image, Input, Popconfirm, Select, Space, Table } from "antd";
 import { Link } from "react-router-dom";
-import { useDelete, useList } from "../../hooks";
+import { useDelete } from "../../hooks";
+import { useState } from "react";
+import { SearchOutlined } from "@ant-design/icons";
+import { useQuery } from "@tanstack/react-query";
+import { axiosClient } from "../../providers";
 
 function ProductList() {
-  const { data, isLoading } = useList({
-    resource: "products",
+  const [searchText, setSearchText] = useState("");
+  const [searchInput, setSearchInput] = useState("");
+  const [filter, setFilter] = useState({
+    category: "",
+    status: "",
+  });
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["products", searchText, filter],
+    queryFn: async () => {
+      const params = [];
+
+      if (searchText)
+        params.push(`name_like=${encodeURIComponent(searchText)}`);
+      if (filter.category)
+        params.push(`category=${encodeURIComponent(filter.category)}`);
+
+      if (filter.status)
+        params.push(`status=${encodeURIComponent(filter.status)}`);
+
+      const queryString = params.length > 0 ? `?${params.join("&")}` : "";
+
+      const { data } = await axiosClient.get(`products${queryString}`);
+      return data;
+    },
   });
   const { mutate } = useDelete({ resource: "products" });
 
+  const options = [
+    {
+      label: "BMW",
+      value: "BMW",
+    },
+    {
+      label: "Audi",
+      value: "Audi",
+    },
+    {
+      label: "Vin",
+      value: "Vin",
+    },
+  ];
+  const statusOptions = [
+    {
+      label: "Active",
+      value: "true",
+    },
+    {
+      label: "Inactive",
+      value: "false",
+    },
+  ];
   const columns = [
     {
       title: "Name",
@@ -33,6 +84,11 @@ function ProductList() {
       key: "desc",
     },
     {
+      title: "category",
+      dataIndex: "category",
+      key: "category",
+    },
+    {
       title: "Actions",
       render: (product: any) => {
         return (
@@ -54,7 +110,42 @@ function ProductList() {
       },
     },
   ];
-  return <Table dataSource={data} columns={columns} loading={isLoading} />;
+  return (
+    <div>
+      <Space>
+        <Space style={{ marginBottom: 16 }}>
+          <Input
+            placeholder="Nhập tên sản phẩm"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            style={{ width: 300 }}
+          />
+          <Button
+            type="primary"
+            icon={<SearchOutlined />}
+            onClick={() => setSearchText(searchInput)}
+          >
+            Tìm kiếm
+          </Button>
+          <Select
+            options={options}
+            allowClear
+            placeholder="Filter Category"
+            style={{ width: "200px" }}
+            onChange={(category) => setFilter({ ...filter, category })}
+          />
+          <Select
+            options={statusOptions}
+            allowClear
+            placeholder="Filter Status"
+            style={{ width: "200px" }}
+            onChange={(status) => setFilter({ ...filter, status })}
+          />
+        </Space>
+      </Space>
+      <Table dataSource={data} columns={columns} loading={isLoading} />;
+    </div>
+  );
 }
 
 export default ProductList;
--- src/providers/index.ts
+++ src/providers/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const token = localStorage.getItem("token");
 
-const axiosClient = axios.create({
+export const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/",
   headers: {
     Authorization: token && `Bearer ${token}`,
